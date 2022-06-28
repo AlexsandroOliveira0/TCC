85c2c947f653d18f13d21253a5042b150005c66f
Alexsandro Araujo <108366189+AlexsandroOliveira0@users.noreply.github.com>
2022-06-28 17:39:32 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function typerWrite(elemento){
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = ' ' ;
+    textoArray.forEach(function(letra, i){
+        setTimeout(function(){
+            elemento.innerHTML += letra;
+        }, 75* i)
+    });
+}
+
+const titulo = document.querySelector('.titulo-principal');
+typerWrite(titulo);
+
